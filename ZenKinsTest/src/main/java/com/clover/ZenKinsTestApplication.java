35ac25293013ccef696cf2baaec7e732f446b07f
Pramod barik <pramod.barik@cloverinfotech.com>
2023-06-26 13:25:41 +0530

    test

--- ZenKinsTest/src/main/java/com/clover/ZenKinsTestApplication.java
+++ ZenKinsTest/src/main/java/com/clover/ZenKinsTestApplication.java
@@ -8,7 +8,7 @@ public class ZenKinsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZenKinsTestApplication.class, args);
-		System.out.println("Application Started......");
+		System.out.println("Application Started...====...");
 	}
 
 }
