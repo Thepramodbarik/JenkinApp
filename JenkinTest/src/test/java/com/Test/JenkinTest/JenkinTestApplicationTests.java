bedfb2c1d5e051bf5f4ce6be8ec8bb38b9df3ae3
Pramod barik <pramod.barik@cloverinfotech.com>
2023-06-27 12:54:07 +0530

    test

---
+++ JenkinTest/src/test/java/com/Test/JenkinTest/JenkinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Test.JenkinTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
